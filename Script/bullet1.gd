229989a52bb97686e4e0eae8c9b672a29d17d9b6
ei1923 <ehigh214@gmail.com>
2025-09-21 13:46:07 +0900

    弾が3秒後に消えるようにした

--- Script/bullet1.gd
+++ Script/bullet1.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 @export var speed = 500
+var lifetime = 3.0 # 寿命を3秒に設定
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	# 3秒後に関数を呼び出すタイマー
+	get_tree().create_timer(lifetime).timeout.connect(on_timer_timeout)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,6 +14,9 @@ func _process(delta: float) -> void:
 	# 弾を前方に移動させる
 	position += transform.x * speed * delta
 
+# 時間がきたら削除される
+func on_timer_timeout():
+	queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
 	# ダメージを受けるものなら
