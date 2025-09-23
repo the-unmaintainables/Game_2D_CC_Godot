6345c9f4ae69e80bb4c157c268272a3bd8d5bfb7
ei1923 <ehigh214@gmail.com>
2025-09-23 15:16:38 +0900

    敵が弾を打つようになった

--- Script/enemy_1.gd
+++ Script/enemy_1.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @export var hp = 1
+@export var bullet_scene: PackedScene
 
 func _physics_process(delta: float) -> void:
 	# 重力処理
@@ -18,7 +19,6 @@ func _physics_process(delta: float) -> void:
 	
 	if get_parent():
 		$AnimatedSprite2D.flip_h = get_parent().reverse_direction
-		#print("Enemyで変数を取得")
 	pass
 
 # ダメージを受ける。死亡処理も記述
@@ -34,3 +34,24 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		#body.damage(1)
 		pass
+
+# 弾を打つ処理
+func fire_bullet():
+	if bullet_scene == null:
+		print("エラー: 弾のシーン")
+		return
+	
+	# 弾のインスタンスを作成
+	var bullet_instance = bullet_scene.instantiate()
+	# 弾の位置を設定
+	bullet_instance.global_position = position
+	# 弾の向きを設定
+	if $AnimatedSprite2D.flip_h:
+		bullet_instance.rotation = deg_to_rad(180)
+	
+	# 現在のシーンに弾を追加
+	self.get_parent().add_child(bullet_instance)
+
+# 弾のクールタイムが終わったことを示すタイマー
+func _on_bullet_timer_timeout() -> void:
+	fire_bullet()
