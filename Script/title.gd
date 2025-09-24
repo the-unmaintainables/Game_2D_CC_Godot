a09a9aaf161e444a6492fcb60f62f1f4ff568880
ei1923 <ehigh214@gmail.com>
2025-09-24 22:00:37 +0900

    タイトルから3秒でステージへ

--- Script/title.gd
+++ Script/title.gd
@@ -4,7 +4,12 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_tree().paused = false
+	# 3秒後に関数を呼び出すタイマー
+	get_tree().create_timer(3).timeout.connect(on_timer_timeout)
 
+func on_timer_timeout():
+	SignalManager.stage_start.emit()
+	GameManager.load_stage1_scene()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
