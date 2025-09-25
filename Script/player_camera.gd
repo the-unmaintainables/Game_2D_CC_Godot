ebb9bb1b2950ce10ee11783e06be430899f33f45
ei1923 <ehigh214@gmail.com>
2025-09-25 21:31:13 +0900

    カメラの領域の設定、両端に壁の追加

--- Script/player_camera.gd
+++ Script/player_camera.gd
@@ -2,6 +2,9 @@ extends Camera2D
 
 @onready var player = self.get_parent().get_node("Player")
 
+@export var limit_L : float
+@export var limit_R : float
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,6 +13,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = player.position
-	position.x = clamp(player.position.x, 0, 9000000000)
+	position.x = clamp(player.position.x, limit_L, limit_R)
 	position.y = 0
 	pass
