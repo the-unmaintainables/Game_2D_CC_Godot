6af287efec6ca046fb6e001ce25265f7ee914105
ei1923 <ehigh214@gmail.com>
2025-09-26 21:38:05 +0900

    モバイル用のUIの変更(同時押し可)

--- Script/mobile_phone_ui.gd
+++ Script/mobile_phone_ui.gd
@@ -11,50 +11,45 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_move_left_button_down() -> void:
-	Input.action_press("move_left")
 
-func _on_move_left_button_up() -> void:
-	Input.action_release("move_left")
-
-
-func _on_move_right_button_down() -> void:
+func _on_move_right_pressed() -> void:
 	Input.action_press("move_right")
 
-func _on_move_right_button_up() -> void:
+func _on_move_right_released() -> void:
 	Input.action_release("move_right")
 
 
-func _on_chage_button_down() -> void:
+func _on_chage_pressed() -> void:
 	Input.action_press("chage")
 
-func _on_chage_button_up() -> void:
+func _on_chage_released() -> void:
 	Input.action_release("chage")
 
 
-func _on_jump_button_down() -> void:
-	Input.action_press("jump")
-
-func _on_jump_button_up() -> void:
-	Input.action_release("jump")
-
-
-func _on_attck_button_down() -> void:
+func _on_attack_pressed() -> void:
 	Input.action_press("attack")
 
-func _on_attck_button_up() -> void:
+
+func _on_attack_released() -> void:
 	Input.action_release("attack")
 
 
-func _on_touch_screen_button_pressed() -> void:
-	Input.action_press("move_right")
+func _on_move_left_pressed() -> void:
+	Input.action_press("move_left")
 
-func _on_touch_screen_button_released() -> void:
-	Input.action_release("move_right")
+func _on_move_left_released() -> void:
+	Input.action_release("move_left")
 
 
-func _on_touch_screen_button_2_pressed() -> void:
+func _on_jump_pressed() -> void:
 	Input.action_press("jump")
 
-func _on_touch_screen_button_2_released() -> void:
+func _on_jump_released() -> void:
 	Input.action_release("jump")
+
+
+func _on_pause_pressed() -> void:
+	Input.action_press("pause")
+
+func _on_pause_released() -> void:
+	Input.action_release("pause")
