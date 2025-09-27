c6992cc07d93e7992b17397b28c5d54e034c12e5
ei1923 <ehigh214@gmail.com>
2025-09-27 20:32:55 +0900

    BGMの追加とループ再生

---
+++ Singletons/bgm_manager.gd
@@ -0,0 +1,9 @@
+extends AudioStreamPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# ゲーム開始時に再生
+	play()
+	print("音の再生")
+	pass # Replace with function body.
